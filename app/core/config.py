528f99638b7223bbe2f625bff3ee7074f8c87431
V4N15H <ikozadaev2018@gmail.com>
2025-10-28 10:08:49 +0400

    Update config.py
    added dotenv support in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,16 @@
 from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = (
-        "postgresql+asyncpg://username:password@host:port/ozon_promo_db"
-    )
-    REDIS_URL: str = "redis://:password@host:port/0"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    REDIS_URL: str = os.getenv("REDIS_URL")
     CELERY_BROKER_URL: str = REDIS_URL
     CELERY_RESULT_BACKEND: str = REDIS_URL
-    OZON_CLIENT_ID: str = "******"
-    OZON_API_KEY: str = "******"
+    OZON_CLIENT_ID: str = os.getenv("OZON_CLIENT_ID")
+    OZON_API_KEY: str = os.getenv("OZON_API_KEY")
 
     class Config:
         env_file = ".env"
